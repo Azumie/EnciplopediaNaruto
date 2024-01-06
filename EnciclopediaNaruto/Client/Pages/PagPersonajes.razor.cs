e090c5afac70094ee47917c37229a0b3cdd6836c
Azumie <azumie250@gmail.com>
2024-01-06 18:22:47 -0400

    Listado de Personajes

---
+++ EnciclopediaNaruto/Client/Pages/PagPersonajes.razor.cs
@@ -0,0 +1,35 @@
+﻿using EnciclopediaNaruto.Shared;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Xml;
+
+namespace EnciclopediaNaruto.Client.Pages
+{
+    public partial class PagPersonajes
+    {
+        List<Personaje> personajes = new List<Personaje>();
+        protected override async Task OnInitializedAsync()
+        {
+            var uri = new Uri("https://narutodb.xyz/api/");
+            var http = new HttpClient() { BaseAddress = uri};
+            var request = await http.GetFromJsonAsync<Page>("character");
+            personajes = request.characters;
+            Console.WriteLine(request.characters.Count());
+            //var responseContent = await request.Content.ReadAsStringAsync();
+
+            // Deserializar la cadena JSON en un objeto C#
+            //var jsonResponse = JsonSerializer.Deserialize<object>(responseContent);
+            //var h = (List<Personaje>)GetPropValue(jsonResponse, "characters");
+            //foreach (var personaje in h)
+            //{
+            //    Console.WriteLine(personaje.name);
+            //}
+            // Imprimir el objeto deserializado en la consola
+            //Console.WriteLine(JsonSerializer.Serialize(jsonResponse, new JsonSerializerOptions { WriteIndented = true }));
+        }
+        public object GetPropValue(object src, string propName)
+        {
+            return src.GetType().GetProperty(propName).GetValue(src, null);
+        }
+    }
+}
---
+++ EnciclopediaNaruto/Shared/Personaje.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnciclopediaNaruto.Shared
+{
+    public class Page
+    {
+        public List<Personaje> characters { get; set; }
+        public int currentPage { get; set; }
+        public int pageSize { get; set; }
+        public int totalCharacters { get; set; }
+    }
+
+    public class Debut
+    {
+        public string ?novel { get; set; }
+        public string ?movie { get; set; }
+        public string ?appearsIn { get; set; }
+    }
+
+    public class Personal
+    {
+        public string ?species { get; set; }
+    }
+
+    public class Personaje
+    {
+        public int id { get; set; }
+        public string ?name { get; set; }
+        public List<string> ?images { get; set; }
+        public Debut ?debut { get; set; }
+        public List<string> ?jutsu { get; set; }
+        public Personal ?personal { get; set; }
+        public List<string> ?uniqueTraits { get; set; }
+    }
+}
